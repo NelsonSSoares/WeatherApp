00517b8b4af256dc1e6b1b6624978399bce98414
Nelson <punkpunkada@gmail.com>
2024-12-08 08:50:58 -0300

    creating search input

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,25 +1,35 @@
+import { Subject, takeUntil } from 'rxjs';
 import { WeatherDatas } from '../../interfaces/WeatherDatas';
 import { WeatherService } from './../../services/weather.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
 
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName= 'São Paulo';
   weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) { }
-
+  // chama o método getWeatherDatas no ngOnInit, quando o componente é inicializado
   ngOnInit(): void {
     this.getWeatherDatas(this.initialCityName);
   }
 
+
+
   getWeatherDatas(cityName: string): void{
     this.weatherService.getWeatherDatas(cityName)
+    .pipe(
+      takeUntil(this.destroy$) // cancela a requisição quando o componente for finalizado, evitando memory leaks
+      // takeUntil é um operador que cancela a requisição quando o observable passado como argumento emite um valor
+    )
     .subscribe({
       next: (response) => {
         // abaixo, o response é atribuído à propriedade weatherDatas
@@ -32,5 +42,16 @@ export class WeatherHomeComponent implements OnInit {
         console.error(error);
       }
     });
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+ // método que será chamado no ngOnDestroy,qunado o componente for finalizado
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
